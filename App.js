8463d0bbcf349ec7f4f06589066653f452d53b91
dbrindha007 <dbrindha007@gmail.com>
2025-07-13 23:14:44 +0530

    Nested react elements

--- App.js
+++ App.js
@@ -1,9 +1,21 @@
- // Create a <h1>Hello Universe!!</h1> element using React
-    const heading = React.createElement('h1', {id : "heading"}, 'Hello Universe!!');
-    console.log(heading);
-    // Get the root DOM node with id="root" and initialize a React root
-    const root = ReactDOM.createRoot(document.getElementById('root'));
-    console.log(root);
-    // Render the React element into the root div
-    root.render(heading);
- 
+/*
+*  Nested React Elements using React.createElement
+*/
+
+const parent = React.createElement('div', { id: "parent" },
+    [React.createElement('div', { className: "child" },
+        React.createElement('h1', null, 'Hello World'),
+        React.createElement('p', null, 'This is a paragraph inside a child div.'))],
+    [
+        React.createElement('div', { className: "child2" },
+            React.createElement('h1', null, 'Hello World2'),
+            React.createElement('p', null, 'This is a paragraph inside a child div.2'))]
+);
+console.log(parent); //Return Object {type: "h1", props: {…}, key: null, ref: null, _owner: null, …}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+console.log(root);
+// Render the React element into the root div
+root.render(parent);
+
+
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-📌 Purpose
+# 📌 Purpose
 This simple project shows how to use React with CDN (without any build tools) to display a heading.
 
 ✅ Steps in the Code
@@ -17,3 +17,23 @@ This simple project shows how to use React with CDN (without any build tools) to
 - We use React.createElement() to create a virtual React element (<h1>Hello Universe!!</h1>).
 
 - Then we attach React to the #root div using ReactDOM.createRoot() and render().
+
+## Who converts the virtual DOM (JS object) into real HTML in React?
+- 👉 ✅ The job is done by ReactDOM, specifically the method:
+- ReactDOM.createRoot(...).render(...)
+
+### Behind the scenes :
+1. You create a virtual DOM (a JS object) using: 
+const heading = React.createElement('h1', null, 'Hello Universe');
+2. You pass it to ReactDOM to render:
+root.render(heading);
+3. ✅ ReactDOM:
+Takes that JS object (virtual DOM)
+
+Compares it with the previous virtual DOM (if any)
+
+Figures out the difference (called diffing 🧠)
+
+Converts it to real DOM nodes (like <h1>Hello Universe</h1>)
+
+Inserts it into the actual browser DOM inside the element you specified (<div id="root">)
